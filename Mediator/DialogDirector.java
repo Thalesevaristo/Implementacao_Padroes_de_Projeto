48a986ec8af123dd162741994bb90510a2c4e819
Thales <35140120+Thalesevaristo@users.noreply.github.com>
2022-06-02 06:00:56 -0300

    Create DialogDirector.java

---
+++ Mediator/DialogDirector.java
@@ -0,0 +1,9 @@
+public abstract class DialogDirector {
+  
+  public void showDialog() {
+  // Código externo  
+  }
+  
+  public abstract void createWidget();
+  public abstract void widgetChanged();
+}
