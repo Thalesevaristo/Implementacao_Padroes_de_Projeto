de05d7a43d1b7e6282449ff80be973434bc7e10c
Thales <35140120+Thalesevaristo@users.noreply.github.com>
2022-06-02 06:24:28 -0300

    Create FontDialogDirector.java

---
+++ Mediator/FontDialogDirector.java
@@ -0,0 +1,27 @@
+public class FontDialogDirector extends DialogDirector {
+  
+  private ListBox lb;
+  private EntryField ef;
+  
+  @Override
+  public void createWidget() {
+    lb = new ListBox(this);
+    ef = new EntryField(this);
+  }
+  
+  @Override
+  public void widgetChanged(Widget w) {
+    lb.getSelection();
+  }
+  
+  public EntryField getEf() {
+    return ef;
+  }
+  
+  public static void main(String[] args) {
+    FontDialogDirector d = new FontDialogDirector();
+    d.createWidget();
+    d.getEf().setText(); // Interação do usuário
+  }
+  
+}
